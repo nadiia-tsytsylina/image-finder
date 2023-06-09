d44ef190cdef31a6e2461ba476047d2025c5601f
Nadiia Tsytsylina <miniova95@gmail.com>
2023-06-09 13:19:00 +0300

    update

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -27,21 +27,25 @@ export default function App() {
   };
 
   useEffect(() => {
+    if (query === '') {
+      return;
+    }
+
     setIsLoading(true);
     setIsShowLoadMore(false);
 
     fetchImages(query, page)
       .then(data => {
-        if (data.totalHits === 0) {
+        const { totalHits, hits } = data;
+
+        if (totalHits === 0) {
           toast.error(`There are no images with tag ${query}`);
           setIsLoading(false);
           setIsShowLoadMore(false);
           return;
         } else {
-          setImages(state =>
-            page === 1 ? data.hits : [...state, ...data.hits]
-          );
-          setIsShowLoadMore(page < Math.ceil(data.totalHits / 12));
+          setImages(state => (page === 1 ? hits : [...state, ...hits]));
+          setIsShowLoadMore(page < Math.ceil(totalHits / 12));
           setIsLoading(false);
         }
       })
@@ -64,63 +68,3 @@ export default function App() {
     </div>
   );
 }
-
-// class oldApp extends Component {
-// state = {
-//   imageName: '',
-//   isLoading: false,
-//   images: null,
-//   page: 1,
-//   isShowLoadMore: false,
-// };
-// async componentDidUpdate(prevProps, prevState) {
-//   const { imageName, page } = this.state;
-//   const prevPage = prevState.page;
-//   const prevImageName = prevState.imageName;
-//   const prevImages = prevState.images;
-//   if (prevImageName !== imageName || prevPage !== page) {
-//     try {
-//       this.setState({ isLoading: true });
-//       const { totalHits, hits } = await fetchImages(imageName, page);
-//       if (totalHits === 0) {
-//         toast.error(`There are no images with tag ${imageName}`);
-//         this.setState({ isLoading: false, isShowLoadMore: false });
-//         return;
-//       } else {
-//         this.setState(prevState => ({
-//           images: page === 1 ? hits : [...prevImages, ...hits],
-//           isShowLoadMore: page < Math.ceil(totalHits / 12),
-//         }));
-//         this.setState({ isLoading: false });
-//       }
-//     } catch (error) {
-//       toast.error(`${error}`);
-//     }
-//   }
-// }
-// handleFormSubmit = imageName => {
-//   if (imageName !== this.state.imageName) {
-//     this.setState({ imageName, page: 1, images: null });
-//   }
-// };
-// handleLoadMore = () => {
-//   this.setState(prevState => ({ page: prevState.page + 1 }));
-// };
-// render() {
-//   const { isLoading, images, isShowLoadMore } = this.state;
-//   return (
-//     <div className={css.App}>
-//       <Searchbar onSubmit={this.handleFormSubmit} />
-//       {images && <ImageGallery images={images} />}
-//       {isShowLoadMore && <Button onCLick={this.handleLoadMore} />}
-//       {isLoading && <Loader />}
-//       <ToastContainer
-//         autoClose={2000}
-//         position="bottom-right"
-//         theme="colored"
-//       />
-//     </div>
-//   );
-// }
-// }
-// export default App;
